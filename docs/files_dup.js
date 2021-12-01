8c7d128b33c6a2432cf8b91a1bb2f2ffc32d0287
rmclaughlin168 <93336468+rmclaughlin168@users.noreply.github.com>
2021-11-30 18:00:06 -0800

    trying again

--- docs/files_dup.js
+++ docs/files_dup.js
@@ -1,4 +1,7 @@
 var files_dup =
 [
-    [ "12 - Fall 2021", "dir_dc7a402aa489f74e99288c1eac494cf6.html", "dir_dc7a402aa489f74e99288c1eac494cf6" ]
+    [ "stat_tracker.cpp", "stat__tracker_8cpp.html", null ],
+    [ "stat_tracker.h", "stat__tracker_8h.html", [
+      [ "StatTracker", "classStatTracker.html", "classStatTracker" ]
+    ] ]
 ];
